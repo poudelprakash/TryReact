1319f80a430e2819e3efab6524d3fbb2d2ee9593
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-03-16 04:00:10 +0545

    demonstrates default value for props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 
-export default class App extends Component{
-    render(){
+export default class App extends Component {
+    render() {
         let txt = this.props.txt;
         return <h1>{txt}</h1>
     }
 }
 
-{/*exprt default const App = () => <h1>Hello stateless</h1>*/}
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+    txt: 'This is default value'
+};
+
+{/*exprt default const App = () => <h1>Hello stateless</h1>*/
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App txt="this is prop text"/>,
+  <App cat={5}/>,
   document.getElementById('root')
 );
