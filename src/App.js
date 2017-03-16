8ec4d11daedf5b654f5e01357939acfc4d585d05
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-03-17 00:34:38 +0545

    listen all events

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
 
 export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {currentEvent: '----'};
+        this.update = this.update.bind(this);
+    }
+
+    update(e) {
+        this.setState({currentEvent: e.type})
+    }
+
     render() {
-        return <Title text="123456"/>
+
+        return <div>
+            <textarea cols="30" rows="10"
+                      onKeyPress={this.update}
+                      onFocus={this.update}
+                      onCut={this.update}
+            ></textarea>
+            <h1>{this.state.currentEvent}</h1>
+        </div>
     }
 }
-
-const Title =(props)=><h1>Title: {props.text}</h1>;
-
-Title.propTypes = {
-    text(props, propName, component){
-     if(!(propName in props)){
-         return new Error(`missing ${propName}`);
-     }
-     if(props[propName].length<6){
-         return new Error(`${propName} was too short`)
-     }
-    }
-};
