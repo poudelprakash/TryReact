4f501702de67145a63d0eddbb9eee7188d337bd3
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-03-17 00:23:17 +0545

    access nested data

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React, {Component} from 'react';
 
 export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            txt: 'this is the state text'
-        }
-    }
-
-    update(e) {
-        this.setState({txt: e.target.value})
-    }
-
     render() {
-        let txt = this.state.txt;
-        return <div>
-            <Widget update={this.update.bind(this)} />
-            <Widget update={this.update.bind(this)} />
-            <Widget update={this.update.bind(this)} />
-            <h1>{txt}</h1>
-            <h2>{this.props.txt}</h2>
-        </div>
+        return <Button>I <Heart /> React</Button>
     }
 }
 
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-};
+const Button = (props)=><button>{props.children}</button>
 
-App.defaultProps = {
-    txt: 'This is default value'
-};
-
-const Widget = (props)=> <input type="text" onChange={props.update}/>
-
-{/*exprt default const App = () => <h1>Hello stateless</h1>*/
+class Heart extends Component{
+    render(){
+        return <span>&hearts;</span>
+    }
 }
