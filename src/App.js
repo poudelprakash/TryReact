c6e6ad74625e2bf6b920262d881804200b4642ff
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-03-17 15:41:47 +0545

    holds re render

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {increasing: false}
+    }
+
     update() {
         ReactDom.render(<App val={this.props.val + 1}/>, document.getElementById('root'))
     }
 
+    componentWillReceiveProps(nextProps) {
+        this.setState({increasing: nextProps.val >  this.props.val})
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.val % 5 === 0;
+    }
+
     render() {
+        console.log(this.state.increasing);
         return (<div>
             <button onClick={this.update.bind(this)}>{this.props.val}</button>
         </div>)
     }
 }
-App.defaultProps = {val: 0}
+App.defaultProps = {val: 0};
 // export default class App extends Component{
 //     constructor(){
 //         super();
