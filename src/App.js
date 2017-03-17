630270e91ae107d51d67458a745ff46ff577d51e
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-03-17 15:36:23 +0545

    rerender..props and states

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 export default class App extends Component {
     update() {
-        ReactDom.render(<App val={this.props.val+1}/>, document.getElementById('root'))
+        ReactDom.render(<App val={this.props.val + 1}/>, document.getElementById('root'))
     }
 
     render() {
@@ -11,5 +11,18 @@ export default class App extends Component {
         </div>)
     }
 }
-
 App.defaultProps = {val: 0}
+// export default class App extends Component{
+//     constructor(){
+//         super();
+//         this.state ={val: 0}
+//     }
+//     update(){
+//         this.setState({val: this.state.val+1})
+//     }
+//     render() {
+//         return (<div>
+//             <button onClick={this.update.bind(this)}>{this.state.val}</button>
+//         </div>)
+//     }
+// }
